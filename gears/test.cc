5062f9cf45565fff79cafc060edc8de5c7510d35
Paul Prozesky <paul.prozesky@gmail.com>
2019-01-02 12:07:59 +0000

    Pedantry and boredom.

--- gears/test.cc
+++ gears/test.cc
@@ -26,9 +26,8 @@ byte search_array(unsigned short new_ratio) {
     unsigned short this_ratio_upper_limit = this_ratio + RATIO_TOLERANCE;
     unsigned short this_ratio_lower_limit = this_ratio - RATIO_TOLERANCE;
     // find it a new home
-    if ((new_ratio > this_ratio_lower_limit) && (new_ratio <= this_ratio_upper_limit)) {
+    if ((new_ratio > this_ratio_lower_limit) && (new_ratio <= this_ratio_upper_limit))
       return MAX_GEARS;  // it matches this one, do nothing
-    }
     else if (new_ratio < this_ratio_lower_limit) {
       lower_match = ctr;  // it's smaller than this one
       break;
@@ -50,7 +49,7 @@ byte search_array(unsigned short new_ratio) {
 void insert_new_ratio(byte new_gear, unsigned short new_ratio) {
   /* Given a new ratio with a new position, but it in the array in RAM
   */
-  if (new_gear >= MAX_GEARS)
+  if (MAX_GEARS <= new_gear)
     return;
   unsigned short old_ratios[MAX_GEARS];
   memcpy(old_ratios, ratios, MAX_GEARS*sizeof(ratios[0]));
@@ -67,39 +66,33 @@ byte update_gear_table(float ratio) {
   /* Given a new ratio, find out where it should be in the list.
   */
   // do we have all of them already?
-  if (num_ratios >= MAX_GEARS) {
+  if (num_ratios >= MAX_GEARS)
     return MAX_GEARS;
-  }
   // otherwise find out where to put this one
   unsigned short new_ratio = ratio * 1000;
   byte new_gear = MAX_GEARS;
-  if (0 == num_ratios) {
+  if (0 == num_ratios)
     new_gear = 0;  // it's the first one
-  }
-  else if (new_ratio > (ratios[num_ratios - 1] + RATIO_TOLERANCE)) {
+  else if (new_ratio > (ratios[num_ratios - 1] + RATIO_TOLERANCE))
     new_gear = num_ratios;  // it's bigger than the current biggest one
-  }
-  else {
+  else
     new_gear = search_array(new_ratio);  // it's somewhere else
-  }
   // update the ratio table as required
-  if (new_gear < MAX_GEARS) {
+  if (new_gear < MAX_GEARS)
     insert_new_ratio(new_gear, new_ratio);
-  }
   return new_gear;
 }
 
-/****************************************************************************************************/
-/* TEST CODE BELOW */
-/****************************************************************************************************/
+/****************************************************************************************************
+  TEST CODE BELOW
+****************************************************************************************************/
 
 void print_ratios(void) {
   /* Print the ratios in RAM
   */
   printf("ratios[");
-  for(int ctr=0; ctr < 9; ctr++) {
+  for(int ctr=0; ctr < 9; ctr++)
     printf("%i, ", ratios[ctr]);
-  }
   printf("]\n");
 }
 
@@ -107,7 +100,8 @@ void reset_ratios(void) {
   /* Reset all the ratios currently stored in RAM.
   Does NOT change EEPROM.
   */
-  for (byte ctr = 0; ctr < MAX_GEARS; ctr++) ratios[ctr] = 65535;
+  for (byte ctr = 0; ctr < MAX_GEARS; ctr++)
+    ratios[ctr] = 65535;
   num_ratios = 0;
 }
 
@@ -116,9 +110,8 @@ void compare_to_expected(unsigned short *expected_ratios, byte num_expected) {
   */
   byte errors = 0;
   for (byte ctr = 0; ctr < num_expected; ctr++) {
-    if (ratios[ctr] != expected_ratios[ctr]) {
+    if (ratios[ctr] != expected_ratios[ctr])
       errors++;
-    }
   }
   if (errors > 0) {
     printf("ERRORS:\n");
