8a9b93263ee21dc66709358837d874828e6686d9
Paul Prozesky <paul.prozesky@gmail.com>
2019-01-02 16:49:37 +0000

    Added some SPI flash chip support for reading status registers and erasing the chip.

--- gears_logger/gears_logger.ino
+++ gears_logger/gears_logger.ino
@@ -2,6 +2,7 @@
 
 #define DEBUG_LOGGING
 
+// constants for this hardware flash logger
 const byte TACH_INTERRUPT_PIN = 2;
 const byte SPEEDO_INTERRUPT_PIN = 3;
 const int ADC_NEUTRAL_PIN = A0;
@@ -19,6 +20,7 @@ const byte DEBOUNCE_TIME = 20;
 const short SHORT_PRESS = 1000;
 const short LONG_PRESS = 5000;
 
+// globals - cos arduinos seem to work like this
 volatile byte logging_enabled = 0;
 volatile byte erase_flag = 0;
 unsigned int ctr_tacho = 0;
@@ -32,13 +34,15 @@ unsigned int adc_neutral = 0;
 volatile byte last_button_reading = 1;
 byte button_state = 1;
 unsigned long debounce_time = 0;
-unsigned long button_event_time = 0;
+unsigned long button_falling_edge_time = 0;
 #define BUTTON_PRESSED (0 == button_state)
 #define BUTTON_RELEASED (1 == button_state)
 
 char debug_string[100];
 
 void setup() {
+  /* This is run once at startup
+  */
   noInterrupts();
   Serial.begin(115200);
   analogReference(DEFAULT);
@@ -53,10 +57,11 @@ void setup() {
 }
 
 void loop() {
-  /* The main loop that the arduino uses for running.
+  /* The main loop that the arduino uses for running
   */
   unsigned long now = millis();
   check_push_button(now);
+  erase_flash();
   update_data(now);
   update_status_led(now);
   dump_data_to_serial();
@@ -68,17 +73,18 @@ void check_push_button(unsigned long time_now) {
   byte last_button_state = button_state;
   read_button_with_debounce(time_now);
   if (1 == last_button_state) {  // button was not pressed
-    button_event_time = BUTTON_PRESSED ? time_now : 0;
+    button_falling_edge_time = BUTTON_PRESSED ? time_now : 0;
   }
-  if ((button_event_time != 0) && BUTTON_PRESSED) {
+  if ((0 != button_falling_edge_time) && BUTTON_PRESSED) {
     // the button is held down and we're counting
-    if (time_now > button_event_time + LONG_PRESS) {
+    if (time_now > button_falling_edge_time + LONG_PRESS) {
       erase_flag = 1;
+      logging_enabled = 0;
       #ifdef DEBUG_LOGGING
       Serial.println("long_press");
       #endif
     }
-    else if (time_now > button_event_time + SHORT_PRESS) {
+    else if (time_now > button_falling_edge_time + SHORT_PRESS) {
       logging_enabled = 1;
       #ifdef DEBUG_LOGGING
       Serial.println("short_press");
@@ -87,25 +93,53 @@ void check_push_button(unsigned long time_now) {
   }
 }
 
-byte read_button_with_debounce(unsigned long time_now) {
-  /* Read the button with a software debounce
+void erase_flash(void) {
+  /* Erase the flash chip.
   */
-  byte button_reading_now = digitalRead(BUTTON_PIN);
-  if (button_reading_now != last_button_reading) {
-    debounce_time = time_now + DEBOUNCE_TIME;
-    last_button_reading = button_reading_now;
-    #ifdef DEBUG_LOGGING
-    Serial.println("debounce_start");
-    #endif
+  if (!erase_flag) return;
+  erase_flag = 0;
+  Serial.println("erasing entire flash chip...");
+  digitalWrite(SPI_CS, LOW);
+  unsigned int result = SPI.transfer(0xc7);
+  unsigned int ctr = 0;
+  while(1 == spi_read_status_reg1() & 0x01) {
+    Serial.print("busy_");
+    Serial.println(ctr);
+    ctr++;
   }
-  else if ((debounce_time != 0) && (time_now > debounce_time)) {
-    debounce_time = 0;
-    button_state = button_reading_now;
-    #ifdef DEBUG_LOGGING
-    sprintf(debug_string, "debounce_done(%1u)", button_state);
-    Serial.println(debug_string);
-    #endif
-  }
+  digitalWrite(SPI_CS, HIGH);
+  Serial.println("done erasing.");
+}
+
+byte spi_read_status_reg1(void) {
+  /* Read status register 1 from the SPI flash chip
+  */
+  digitalWrite(SPI_CS, LOW);
+  SPI.transfer(0x05);
+  byte result = SPI.transfer(0x00);
+  digitalWrite(SPI_CS, HIGH);
+  return result;
+}
+
+byte spi_read_status_reg2(void) {
+  /* Read status register 1 from the SPI flash chip
+  */
+  digitalWrite(SPI_CS, LOW);
+  SPI.transfer(0x35);
+  byte result = SPI.transfer(0x00);
+  digitalWrite(SPI_CS, HIGH);
+  return result;
+}
+
+unsigned short spi_read_status_reg_write(void) {
+  /* Read the write status register from the SPI flash chip
+  */
+  digitalWrite(SPI_CS, LOW);
+  SPI.transfer(0x01);
+  byte result_lsb = SPI.transfer(0x00);
+  byte result_msb = SPI.transfer(0x00);
+  digitalWrite(SPI_CS, HIGH);
+  return result_lsb + (result_msb << 8);
 }
 
 void update_status_led(unsigned long time_now){
@@ -135,12 +169,13 @@ void update_data(unsigned long time_now){
 void dump_data_to_serial(void){
   /* Dump all the data in the EEPROM to the serial port
   */
-  // read the data out if necessary
   if(Serial.available() > 4) {
     Serial.setTimeout(UPDATE_RATE);
     String str = Serial.readStringUntil('\n');
     if(str.substring(0) == "dump") {
       Serial.println("Printing all flash data to serial.");
+    }
+    else if(str.substring(0) == "query") {
       digitalWrite(SPI_CS, LOW);
       sprintf(debug_string, "byte_read(0x%02x)", SPI.transfer(0x00));
       Serial.println(debug_string);
@@ -151,7 +186,7 @@ void dump_data_to_serial(void){
       unsigned int result3 = SPI.transfer(0x00);
       unsigned int result4 = SPI.transfer(0x00);
       unsigned int result5 = SPI.transfer(0x00);
-      sprintf(debug_string, "0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x", result0, result1, result2, result3, result4, result5);
+      sprintf(debug_string, "flash_info: 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x", result0, result1, result2, result3, result4, result5);
       Serial.println(debug_string);
       digitalWrite(SPI_CS, HIGH);
     }
@@ -199,6 +234,27 @@ void isr_speedo() {
   /* Just increment the speedo counter
   */
   ctr_speedo++;
+}
+
+byte read_button_with_debounce(unsigned long time_now) {
+  /* Read the button with a software debounce
+  */
+  byte button_reading_now = digitalRead(BUTTON_PIN);
+  if (button_reading_now != last_button_reading) {
+    debounce_time = time_now + DEBOUNCE_TIME;
+    last_button_reading = button_reading_now;
+    #ifdef DEBUG_LOGGING
+    Serial.println("debounce_start");
+    #endif
+  }
+  else if ((debounce_time != 0) && (time_now > debounce_time)) {
+    debounce_time = 0;
+    button_state = button_reading_now;
+    #ifdef DEBUG_LOGGING
+    sprintf(debug_string, "debounce_done(%1u)", button_state);
+    Serial.println(debug_string);
+    #endif
+  }
 }
 
 // end
